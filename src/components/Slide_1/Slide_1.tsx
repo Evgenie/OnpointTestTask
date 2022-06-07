49758011ad6968561152bc994515667a55bf73e5
Evgenie <nestroykozni@gmail.com>
2022-06-07 15:18:16 +0300

    fixed prettier / eslint conflict

---
+++ src/components/Slide_1/Slide_1.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Slide_1 = () => {
+	return <div>Slide_1</div>;
+};
+
+export default Slide_1;
---
+++ src/components/Slide_1/index.ts
@@ -0,0 +1,5 @@
+import Slide_1 from "./Slide_1";
+
+export { Slide_1 };
+
+export default Slide_1;
